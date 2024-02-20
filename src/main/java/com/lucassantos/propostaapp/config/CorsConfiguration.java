95cd677ad604ae7f4f8e0416d9bf6b88fb7ed635
Lucas Santos <lucas201273@gmail.com>
2024-02-20 12:03:26 -0300

    feat: add rabbitmq on docker-compose

--- src/main/java/com/lucassantos/propostaapp/config/CorsConfiguration.java
+++ src/main/java/com/lucassantos/propostaapp/config/CorsConfiguration.java
@@ -1,13 +1,22 @@
-package com.lucassantos.propostaapp.cconfig;
+package com.lucassantos.propostaapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*");
+    }
+
+    @Bean
+    public Locale localizacao(){
+        return Locale.US;
     }
 }
